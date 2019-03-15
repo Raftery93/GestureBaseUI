414169f36c382c2a802fb78ab198031877f79d42
Gary Connelly <G00336837@gmit.ie>
2019-03-15 15:37:40 +0000

    Attatchiny Myo movements to the Player_Move script.

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -11,6 +11,7 @@ public class Player_Move : MonoBehaviour
     // Myo game object to connect with.
     // This object must have a ThalmicMyo script attached.
     public GameObject myo = null;
+    public GameObject player;
 
     private int playerSpeed;
     public static bool facingRight;
@@ -37,7 +38,8 @@ public class Player_Move : MonoBehaviour
     //Move player
     void PlayerMove()
     {
-        KeyBoardMovement(); // Trigger keyboard controlls method.
+        //KeyBoardMovement(); // Trigger keyboard controlls method.
+        MyoMovements();
     }
 
     void KeyBoardMovement()
@@ -64,6 +66,72 @@ public class Player_Move : MonoBehaviour
         moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y); // Move the player.
     }
 
+    void MyoMovements()
+    {
+
+        playerSpeed = 5; // Set the player speed to 10.
+        playerJumpPower = 70;
+
+        // Access the ThalmicMyo component attached to the Myo game object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+
+        // Vibrate the Myo armband when a fist is made.
+        if (thalmicMyo.pose == Pose.Fist)
+        {
+           
+            Jump();
+
+          //  ExtendUnlockAndNotifyUserAction (thalmicMyo);
+
+           
+        }
+        else if (thalmicMyo.pose == Pose.WaveIn)
+        {
+            
+            moveX = -1.0f;
+            
+
+            ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        }
+        else if (thalmicMyo.pose == Pose.WaveOut)
+        {
+           
+            moveX = 1.0f;
+           
+
+            ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        }
+        else if (thalmicMyo.pose == Pose.DoubleTap)
+        {
+
+            
+            Debug.Log("Double tap.");
+
+            ExtendUnlockAndNotifyUserAction(thalmicMyo);
+        }
+        else if (thalmicMyo.pose == Pose.Rest)
+        {
+           
+            moveX = 0.0f;
+        }
+
+
+        // Player direction.
+        if (moveX < 0.0f && facingRight == false) // So the player is facing left when moving left.
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true) // So the player is facing right when moving right.
+        {
+            FlipPlayer();
+        }
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(
+        moveX * playerSpeed, player.GetComponent<Rigidbody2D>().velocity.y); // Move the player.
+        Debug.Log("Moving X ---> " + moveX);
+
+    }
+
     private void FlipPlayer()
     {
         // Flip code.
@@ -79,11 +147,24 @@ public class Player_Move : MonoBehaviour
     {
        
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower); // Character moves up.
-          //  isGrounded = false; // Character is not grounded and therefore cannot jump.
-
-        
-        
+          //  isGrounded = false; // Character is not grounded and therefore cannot jump. 
     }
+
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
+    }
+
 
 }
 
