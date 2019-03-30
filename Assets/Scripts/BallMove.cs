608b0e1cbe15f658f8a473658d9b97a5209c9738
Gary Connelly <G00336837@gmit.ie>
2019-03-30 13:59:53 +0000

    Level three mechanics working.

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -40,7 +40,17 @@ public class BallMove : MonoBehaviour
             {
                 //Do nothing.
             }
-            else
+            else if (sceneName == "Bubble3" && nextBall.name == "Ball2")
+            {
+                GameObject ball1 = Instantiate(nextBall, rb.position + Vector2.right / 4f, Quaternion.identity);
+                GameObject ball2 = Instantiate(nextBall, rb.position + Vector2.left / 4f, Quaternion.identity);
+                GameObject ball3 = Instantiate(nextBall, rb.position + Vector2.left / 4f, Quaternion.identity);
+
+                ball1.GetComponent<BallMove>().startForce = new Vector2(2f, 5f);
+                ball2.GetComponent<BallMove>().startForce = new Vector2(-2f, 5f);
+                ball3.GetComponent<BallMove>().startForce = new Vector2(-3f, 5f);
+            }
+            else // The default is level Bubble2.
             {
                 GameObject ball1 = Instantiate(nextBall, rb.position + Vector2.right / 4f, Quaternion.identity);
                 GameObject ball2 = Instantiate(nextBall, rb.position + Vector2.left / 4f, Quaternion.identity);
