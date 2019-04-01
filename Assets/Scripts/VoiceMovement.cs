2105a983a94d30342c47c41da123729d053fb37c
conor <c-raftery@hotmail.com>
2019-04-01 20:59:10 +0100

    VoiceMovement done

--- Assets/Scripts/VoiceMovement.cs
+++ Assets/Scripts/VoiceMovement.cs
@@ -12,11 +12,30 @@ public class VoiceMovement : MonoBehaviour
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
     void Start(){
-        actions.Add("forward", Forward);
-        actions.Add("up", Up);
-        actions.Add("down", Down);
-        actions.Add("back", Back);
+        actions.Add("right", Right);
+        actions.Add("left", Left);
+        //actions.Add("down", Down);
+        //actions.Add("back", Back);
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+
+        keywordRecognizer.Stop();
+
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech){
+
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Right(){
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Left(){
+        transform.Translate(-1, 0, 0);
     }
 }
