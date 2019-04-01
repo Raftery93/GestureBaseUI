454d6f2a5feee97c9dc699e5385ebe1f7a191157
conor <c-raftery@hotmail.com>
2019-04-01 20:51:34 +0100

    Configured keyword recognizer

--- Assets/Scripts/VoiceMovement.cs
+++ Assets/Scripts/VoiceMovement.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
+using System;
+using System.Linq;
 
 public class VoiceMovement : MonoBehaviour
 {
     private KeywordRecognizer keywordRecognizer;
 
-    
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    void Start(){
+        actions.Add("forward", Forward);
+        actions.Add("up", Up);
+        actions.Add("down", Down);
+        actions.Add("back", Back);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+    }
 }
